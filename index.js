aaee24d81075cd1d8c45eba72869505a937d4615
Darian Fultz <dfultz826@yahoo.com>
2022-11-09 14:12:38 -0500

    Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+let mondayWork = function(activity2 = "go to the office") {
+    return `This Monday, I will ${activity2}.`;
+}
+
+let wrapAdjective = function(flair = "*"){
+    return function(singleP = "special"){
+        return `You are ${flair}${singleP}${flair}!`
+    }
+}
